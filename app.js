3cd19f18df6236f8775bf3a891c25a13e693f0e2
loganmurphy <loganmurphy1984@gmail.com>
2017-11-11 17:21:44 -0600

    fixed a typo

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ io.on('connection', function(socket){
 
   })
 });
-
-http.listen(3000, function(){
-  console.log('Now up on PORT 3000')
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log('Now up on PORT 3000');
 });
