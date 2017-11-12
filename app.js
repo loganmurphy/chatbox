afd07e73e2e95dd6cdf4ca1e329dae3470ea96bb
loganmurphy <loganmurphy1984@gmail.com>
2017-11-12 00:19:21 -0600

    fixed a typo

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ var db = require('./models');
 var authorizationURL = "https://accounts.google.com/o/oauth2/auth";
 var clientID = authConfig.web.client_id;
 
+
+var env = process.env.NODE_ENV || ‘development’;
+var config = require(__dirname + ‘/../config/config.json’)[env];
+var db = {};
+if (config.use_env_variable) {
+   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+
 const body_parser = require('body-parser');
 
 
